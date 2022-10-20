4dff7c8fd887b19b67210fb1f4b5bbbe6a5ddf8d
Elio <eliochen34@gmail.com>
2022-10-20 09:57:12 +0800

    add comments

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,7 +6,7 @@ const Account = require('../../models/account')
 
 
 router.get('/', (req, res) => {
-  // 判斷cookie中儲存的user內的值，如果有值則該使用者不用再輸入帳號密碼，可以直接登入。若無則導至登入頁面
+  // 判斷cookie中儲存的user內的值，如果有值則該使用者不用再輸入帳號密碼，可以直接登入。若無則導至登入頁面讓使用者輸入帳號密碼。
   const { user } = req.signedCookies
   // console.log(user)
   if (user) {
